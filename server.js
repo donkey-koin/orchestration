774eed03b1392df28ed9c4ecde2e223cb29bcdf9
sowakarol <karosowa@student.agh.edu.pl>
2018-04-21 13:37:23 +0200

    Connection between transaction-service's endpoint established with axios

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 import express from "express";
+import axios from "axios";
 
 const app = express();
 const port = process.env.PORT || 5000;
+const url =
+  "http://localhost:8080/values";
+
 
 app.get('/api', (req, res) => {
-  res.send({ express: 'Hallo' });
+  axios
+  .get(url)
+  .then(response => {
+    res.send(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+    res.send({express:'Hallo'});
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
