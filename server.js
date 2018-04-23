756f2665b8f8332db87d49976ba3464878ff6cd6
sowakarol <karosowa@student.agh.edu.pl>
2018-04-24 01:00:46 +0200

    Add moment.js with utc dates simple searching

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import axios from "axios";
+import moment from "moment";
 
 const app = express();
 const port = process.env.PORT || 5000;
 const url =
-  "http://localhost:8080/values";
+    "http://localhost:8080/values";
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,18 +14,28 @@ app.use(bodyParser.json());
 var routes = require('./api/routes/routes'); //importing route
 routes(app);
 
-app.get('/api', (req, res) => {
-  axios
-  .get(url)
-  .then(response => {
-    res.send(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-    res.send({express:'Hallo'});
-  });
+app.get('/api/values', (req, res) => {
+    if (Number(req.query.last)) {
+
+        var today = moment().set({ second: 0 });
+        console.log(today.utc().format());
+
+        axios
+            .get(url + "/search?date=" + today.utc().format())
+            .then(response => {
+                res.send(response.data);
+            })
+    } else {
+        axios
+            .get(url)
+            .then(response => {
+                res.send(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+                res.send({ express: 'Hallo' });
+            });
+    }
 });
 
-app.listen(port);
-
-console.log('Orchestration server started on: ' + port);
+app.listen(port, () => console.log(`Listening on port ${port}`));
