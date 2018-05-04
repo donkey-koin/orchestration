85dec8d88ebb176d26818ae2cb75d595aeb0d3d9
sowakarol <karosowa@student.agh.edu.pl>
2018-05-04 19:53:35 +0200

    cors header firefox fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
